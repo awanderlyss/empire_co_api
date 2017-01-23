a08da0c3571f85ff5ac7f6b8b887ca70d25e6d97
yourUsername <your_email@example.com>
2017-01-23 16:59:04 -0500

    create home route

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,14 @@
 // fetching the product model
 var Product = require('./models/product');
-
+var express = require('express');
+var router = express.Router()
 module.exports = function(app) {
-  
+  router.use(function(req, res, next){
+    console.log("Something is happening");
+
+  });
+  router.get("/", function(req, res){
+    res.json({message:"Welcome to Empire Co"});
+  });
+  app.use("/",router)
 };
