5fae899128eb040ebd4c7b9a243aa8e73afa7866
awanderlyss <alyssa.felix7@gmail.com>
2017-01-24 20:22:13 -0500

    Add routes for passport

--- app/routes.js
+++ app/routes.js
@@ -65,13 +65,43 @@ module.exports = function(app, passport) {
     });
 // end of apiRouter routes =====================================
 
-// loginRouter routes ==========================================
+// passport routes ==========================================
+  // login
+  app.get('/login', function(req, res) {
+    res.json({ message: req.flash('loginMessage') });
+  });
+  // signup
+  app.get('/signup', function(req, res) {
+    res.json({ message: req.flash('signupMessage') });
+});
+
+  // process the signup form
+  // app.post('/signup', do all our passport stuff here);
+
+  // profile of logged in user
+  app.get('/profile', isLoggedIn, function(req, res) {
+      res.json( {user : req.user });// get the user out of session
+  });
+
+  // logout
+  app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
 
 
-
-// end of loginRouter routes ===================================
+// end of passport routes ===================================
 
     // register routes =========================================
     app.use('/api', apiRouter);
 
 };
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated())
+        return next();
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
